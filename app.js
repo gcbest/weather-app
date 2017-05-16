523d7f2b97f9c553ac68721c783bac38880fe0db
Godfrey Best <gcb246@stern.nyu.edu>
2017-05-15 20:02:11 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
+
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Address to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
+
+
+geocode.geocodeAddress(argv.a, (errorMessage, result) => {
+	if (errorMessage) {
+		console.log(errorMessage);
+	}
+	else {
+		console.log(JSON.stringify(result, undefined, 2));
+	}
+});	
+
---
+++ playground/async-basics.js
@@ -0,0 +1,11 @@
+console.log('Starting App');
+
+setTimeout(() => {
+	console.log('Delay')
+}, 2000);
+
+setTimeout(() => {
+	console.log('No Delay')
+}, 0);
+
+console.log('Finishing Up');
